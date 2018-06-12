893beefa2635d53d21d00eefeda2d3757bb896b8
Vili Nikkola <vili.nikkola@vincit.com>
2018-06-12 15:03:20 +0300

    change navigation bar to normal from fixed

--- src/NavigationBar/styles.js
+++ src/NavigationBar/styles.js
@@ -9,8 +9,6 @@ export const NavigationBar = styled.div`
   justify-content: space-between;
   align-items: center;
   background-color: ${p => p.bgcolor ? p.bgcolor : 'none'};
-  position: fixed;
-  top: 0;
 `
 
 export const MenuLinks = styled.div`
