72c983b9bb66bc5833aa091168310f4cca57eb98
Toni Lehtimaki <toni@redland.fi>
2018-06-12 14:45:39 +0300

    add withInfo

--- src/Hero/Hero.stories.js
+++ src/Hero/Hero.stories.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import { withInfo } from '@storybook/addon-info';
 
 import Hero from '.'
 
@@ -7,11 +8,11 @@ import heroBg from '../../assets/dsconf-wide.jpg';
 import heroLogo from '../../assets/dsconf-logo.png';
 
 storiesOf('Hero', module)
-  .add('index', () => (
+  .add('index', withInfo(`Hero is hero`)(() => (
     <Hero 
       title='A Design Systems conference'
       subTitle={'March 14th - 15th, 2018'}
       subTitle2={'In Helsinki, Finland'}
       background={heroBg}
       logoImage={heroLogo} />
-  ))
+  )))
